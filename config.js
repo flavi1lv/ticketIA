07e4f6368a282110b86a35ee057a635c7db7fa33
Flavi1 <p.flavien07@gmail.com>
2026-04-05 21:44:15 +0200

    ajout groq api

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 require('dotenv').config({ quiet: true });
 
 module.exports = {
-  aiProvider: 'gemini', //"gemini" ou "ollama"
-  geminiApiKey: process.env.GEMINI_API_KEY
+  aiProvider: 'groq', //"groq (ollama en cloud)" ou "ollama"
+  geminiApiKey: process.env.GEMINI_API_KEY,
+  groqApiKey: process.env.GROQ_API_KEY
 };
 
